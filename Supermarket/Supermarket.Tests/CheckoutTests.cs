0fe9d1bb8d1ca1b329199b15beeea992a4c847c8
Gareth Griffiths <gareth.griffiths@nielsen.com>
2021-12-08 18:58:04 +0000

    Initial solution added

---
+++ Supermarket/Supermarket.Tests/CheckoutTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Supermarket.Tests
+{
+    [TestClass]
+    public class CheckoutTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
